ea3141382e9c4d4e0144df55f5aa734ff93adda2
unknown <TrenksSS>
2023-03-07 11:19:51 -0300

    ultima

---
+++ 01-proj/ProjAgro/front/script.js
@@ -0,0 +1,8 @@
+function abrirmodal1() {
+    let card = document.querySelector("#card1")
+    card.style = "display:flex"
+}
+function fecharmodal1() {
+    let card = document.querySelector("#card1")
+    card.style = "display:none"
+}
